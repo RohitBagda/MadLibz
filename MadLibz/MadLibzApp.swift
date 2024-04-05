dc5bbe355bbb175e013614008134594c15d618dc
RohitBagda <rohitbagda@gmail.com>
2024-04-04 21:53:40 -0500

    Initial Commit

---
+++ MadLibz/MadLibzApp.swift
@@ -0,0 +1,17 @@
+//
+//  MadLibzApp.swift
+//  MadLibz
+//
+//  Created by Rohit Bagda on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MadLibzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
